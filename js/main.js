bdbc6190bbde364e3b23463ac81189f14828bb2f
Loren Burkholder <55629213+LorenDB@users.noreply.github.com>
2019-11-17 18:23:27 -0500

    mobile fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-    document.getElementById("menu").style.left = "-380px";
+    document.getElementById("menu").style.left = "-120%";
     document.getElementById("siteCoverForMenu").style.left = "-120%";
     //alert("width: " + window.innerWidth + "\nheight: " + window.innerHeight);
 };
 function toggleMenu()
 {
     var menu = document.getElementById("menu");
-    if (menu.style.left == "-380px")
+    if (menu.style.left == "-120%")
         menu.style.left = "0px";
-    else menu.style.left = "-380px";
+    else menu.style.left = "-120%";
     var cover = document.getElementById("siteCoverForMenu");
     if (cover.style.left == "-120%")
         cover.style.left = "0px";
