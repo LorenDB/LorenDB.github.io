c4c6cbb012406697a72b466c5ecdad2795e039b8
Loren Burkholder <55629213+LorenDB@users.noreply.github.com>
2019-11-16 21:06:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ window.onload = function() {
 function toggleMenu()
 {
     var menu = document.getElementById("menu");
-    if (menu.style.left == "-365px")
+    if (menu.style.left == "-380px")
         menu.style.left = "0px";
-    else menu.style.left = "-365px";
+    else menu.style.left = "-380px";
     var cover = document.getElementById("siteCoverForMenu");
-    if (cover.style.left == "-100%")
+    if (cover.style.left == "-120%")
         cover.style.left = "0px";
-    else cover.style.left = "-100%";
+    else cover.style.left = "-120%";
 }
